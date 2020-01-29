d623c874247152bd12da260dd5ac44ae24be5874
Gerardo González <wagegosa@gmail.com>
2020-01-29 07:14:59 -0500

    Se actualiza a español el index de introducción. Se ingresa modulo de Varibles.

---
+++ 2-Variables/1-variables.js
@@ -0,0 +1,18 @@
+//Variable tipo String
+var name = "Walter Gerardo";
+console.log("1:", name, "No duerme mucho.");
+
+//Variables Number
+var number = 5;
+console.log("2:",number);
+
+//Contenido actualizar
+number = 3.14;
+console.log("3:", number);
+
+var number2 = "2";
+console.log("4:", number2);
+
+console.log("5:", typeof number2);
+console.log("6:", typeof number);
+
