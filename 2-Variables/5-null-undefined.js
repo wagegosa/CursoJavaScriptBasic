5e8dfdb89d59404719fb4cf0e5c9f5b552e46ebb
Gerardo González <wagegosa@gmail.com>
2020-02-15 17:19:12 -0500

    Se actualiza el null o indefinido.

---
+++ 2-Variables/5-null-undefined.js
@@ -0,0 +1,18 @@
+var variableSinValor;
+console.log("1: variable sin asignacion", variableSinValor);
+
+var cadenaVacia = "";
+console.log("2.0: es un string?",  cadenaVacia);
+console.log("2.1: es un string? ", typeof cadenaVacia);
+
+var humano = {
+  nombre: "saitama",
+  edad: 28
+};
+console.log("3: acceder a una propiedad inexistente", humano.correo);
+
+var animales = ["lobo", "leon"];
+console.log("4: acceder a una posicion inexistente", animales[2]);
+
+var valorNulo = null;
+console.log("5: vairable con valor nulo", valorNulo);
