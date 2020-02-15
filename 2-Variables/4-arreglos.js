4caea1c25cca6c74d797b8d129735c59bd11ca8d
Gerardo González <wagegosa@gmail.com>
2020-02-15 14:31:30 -0500

    Se agrega archivo de arreglos.

--- 2-Variables/4-arreglos.js
+++ 2-Variables/4-arreglos.js
@@ -1,55 +1,56 @@
 var animales = ["lobo", "leon", "dragon"];
 console.log("1: arreglo de animales", animales);
 
-// console.log("2: imprimir un elemento en especifico", animales[0]);
+console.log("2: imprimir un elemento en especifico", animales[0]);
+console.log("3: imprimir un elemento en especifico", animales[1]);
+console.log("4: imprimir un elemento en especifico", animales[2]);
 
 // // actualizar contenido
-// animales[0] = "ciervo";
-// console.log("3: contenido actualizado", animales[0]);
+animales[0] = "ciervo";
+console.log("5: contenido actualizado", animales[0]);
 
-// var numeros = [1, 2, 3];
+var numeros = [1, 2, 3];
+console.log("6: arreglo de numeros", numeros);
 
-// console.log("4: arreglo de numeros", numeros);
+var arregloMixto = [1, "perro", true];
+console.log("5: arreglo mixto", arregloMixto);
 
-// var arregloMixto = [1, "perro", true];
-// console.log("5: arreglo mixto", arregloMixto);
+var usuarios = [
+  // objeto 1
+  {
+    nombre: "jorge",
+    edad: 18
+  },
+  // objeto 2
+  {
+    nombre: "joel",
+    edad: 26
+  },
+  // objeto 3
+  {
+    nombre: "juan",
+    edad: 32
+  }
+];
+console.log("6: arreglo de objetos", usuarios);
 
-// var usuarios = [
-//   // objeto 1
-//   {
-//     nombre: "jorge",
-//     edad: 18
-//   },
-//   // objeto 2
-//   {
-//     nombre: "joel",
-//     edad: 26
-//   },
-//   // objeto 3
-//   {
-//     nombre: "juan",
-//     edad: 32
-//   }
-// ];
-// console.log("6: arreglo de objetos", usuarios);
+var usuarios = [
+  // objeto 1
+  {
+    nombre: "jorge",
+    edad: 18,
+    tieneHijos: false
+  },
+  // objeto 2
+  {},
+  // objeto 3
+  {
+    nombre: "juan",
+    edad: 32
+  }
+];
 
-// var usuarios = [
-//   // objeto 1
-//   {
-//     nombre: "jorge",
-//     edad: 18,
-//     tieneHijos: false
-//   },
-//   // objeto 2
-//   {},
-//   // objeto 3
-//   {
-//     nombre: "juan",
-//     edad: 32
-//   }
-// ];
-
-// console.log("7: arreglo de objetos con diferentes estructuras:", usuarios);
+console.log("7: arreglo de objetos con diferentes estructuras:", usuarios);
 // console.log(
 //   "8: acceder a la propiedad nombre del elemento en la posicion 0:",
 //   usuarios[0].tieneHijos
