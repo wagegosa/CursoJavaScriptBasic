330ae83e9699f53360a5a4fc322b6b90353227c7
Gerardo González <wagegosa@gmail.com>
2020-02-16 23:33:32 -0500

    Se termina ejercicio de Asignaciones.

---
+++ 3-Operadores/3-asignacion.js
@@ -0,0 +1,39 @@
+// asignacion basica operador =
+var a = 5;
+console.log("1:", a);
+
+// actualizar el valor de x usando su mismo valor
+// se resuelven primero los operadores aritmeticos
+a = a + 1;
+console.log("2:", a);
+
+// asignacion +=
+var b = 4;
+console.log("Valor inicial de 'b' es: ", b);
+b += 2; // <= b = b + 2;
+console.log("3: con la forma +=, el resultado es: ", b);
+
+// asignacion -=
+b -= 2; // <= b = b - 2;
+console.log("4: Con la forma -=, el resultado es: ", b);
+
+// asignacion *=
+var c = 5;
+console.log("El valor inicial de 'c' es de: ", c);
+c *= 2; // <= c = c * 2;
+console.log("5: Con la forma *=, el resultado es: ", c);
+
+// asignacion /=
+c /= 2; // <= c = c / 2;
+console.log("6: Con la forma /=, el resultado es: ", c);
+
+// asignacion %=
+var d = 10;
+console.log("El valor inicial de 'd' es de: ", d);
+d %= 2; // <= d = d % 2;
+console.log("7: Con la forma %=2, el resultado es: ", d);
+
+var e= 10;
+console.log("El valor inicial de 'e' es de: ", e);
+e %= 3;
+console.log("8: Con la forma %=3, el resultado es: ", e);
