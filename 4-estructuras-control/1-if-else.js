c97caf506a182cc2819ebdbb6ceb9f5b3a296550
Gerardo González <wagegosa@gmail.com>
2020-02-17 18:35:12 -0500

    Se termina el archivo de if y else.

---
+++ 4-estructuras-control/1-if-else.js
@@ -0,0 +1,53 @@
+if (false) {
+  console.log("1: Hola mundo!");
+} else {
+  console.log("1: Hola");
+}
+
+// var hayDescuento = true;
+var hayDescuento = false;
+
+if (hayDescuento) {
+  console.log("2: Todavia hay descuentos!");
+}else{
+  console.log("2: No hay descuentos!");
+}
+
+// var horaTrabajo = false;
+var horaTrabajo = true;
+
+if (horaTrabajo) {
+  console.log("3: A trabajar");
+} else {
+  console.log("3: A descansar");
+}
+
+// numeros
+a=10;
+b =2
+if (a % b = 0) {
+  console.log("4: Es par");
+} else {
+  console.log("4: Es impar");
+}
+
+// strings
+if ("hola") {
+  console.log("5: cadena no vacia");
+} else {
+  console.log("5: esta vacio");
+}
+
+// objetos
+if (null) {
+  console.log("5: es un objeto");
+} else {
+  console.log("5: es un null");
+}
+
+// undefined
+if (true) {
+  console.log("6: no es un undefined");
+} else {
+  console.log("6: es un undefined");
+}
