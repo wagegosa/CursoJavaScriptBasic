26086aa69cde0f14cd15e4b6b799c5d37c460531
Gerardo González <wagegosa@gmail.com>
2020-02-16 22:57:45 -0500

    Se termina el archivo de modulo.

---
+++ 3-Operadores/2-modulo.js
@@ -0,0 +1,14 @@
+// % es el operdador modulo
+// Proporciona las sobras.
+var sobra = 5 % 4;
+console.log("1: sobra", sobra);
+
+console.log("2:", 1 % 4);
+
+console.log("3:", 4 % 2);
+
+console.log("4:", 5 % 2);
+
+console.log("5:", 11 % 2);
+
+console.log("6:", 457 % 2);
