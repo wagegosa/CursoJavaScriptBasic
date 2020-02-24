53e654f1d7cdaa07deffbdced4c98a6451df4b50
Gerardo González <wagegosa@gmail.com>
2020-02-24 15:12:56 -0500

    Se Termina condicionales de operadores.

---
+++ 4-estructuras-control/2-operadores-comparacion-1.js
@@ -0,0 +1,33 @@
+//var edad = 18;
+var edad = 17;
+
+// operador mayor que >
+if (edad > 17) {
+  console.log("1: puede votar");
+} else {
+  console.log("1: no puede votar");
+}
+
+// operador mayor o igual que >=
+if (edad >= 18) {
+  console.log("2: puede votar");
+} else {
+  console.log("2: no puede votar");
+}
+
+//var precioFinal = 200;
+var precioFinal = 100;
+
+// operador menor que <
+if (precioFinal < 200) {
+  console.log("3: Aplica descuento");
+} else {
+  console.log("3: No aplica descuento");
+}
+
+// operador menor o igual que <=
+if (precioFinal <= 200) {
+  console.log("4: Aplica descuento");
+} else {
+  console.log("4: No aplica descuento");
+}
