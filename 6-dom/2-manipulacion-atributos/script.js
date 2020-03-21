7eff906a1cba09144d02b390a9fed51cc8e57410
Gerardo González <wagegosa@gmail.com>
2020-03-20 20:32:28 -0500

    Se termina ejercicios en los cuales agregamos, modificamos y eliminamos vaores de una clase.

--- 6-dom/2-manipulacion-atributos/script.js
+++ 6-dom/2-manipulacion-atributos/script.js
@@ -1,27 +1,34 @@
 var link = document.getElementById("link");
-/*
+
 // verificar si existe el atributo
+// el atributo "hasAttribute" sirve para verificar que contengan "href"  
 if (link.hasAttribute("href")) {
   // obtener valor del atributo
-  console.log("1: valor del atributo", link.getAttribute("href"));
+  // con "getAttribute" obtenemos el atributo del elimento pasando el parametro "href"
+  console.log("1: El valor del atributo es:", link.getAttribute("href"));
 }
 
 // agregar atributo con valor
-link.setAttribute("class", "verde");
+// con este atributo le agregamos una classs con el valor verde
+// link.setAttribute("class", "verde");
+link.setAttribute("class", "azul")
 
-console.log("2: enlace", link);
+console.log("2.1: enlace: ", link);
+// mostramos los atributos que llamamos
 console.log("3: atributos", link.attributes);
 
 // eliminar atributo
-link.removeAttribute("class");
-console.log("4: atributos", link.attributes);
+// link.removeAttribute("class");
+// console.log("4: atributos", link.attributes);
 
 // control de clases
 link.classList.add("rojo");
-link.classList.add("roboto");
+link.classList.add("verde");
+link.classList.add("container");
 console.log("5: clases", link.classList);
 
-link.classList.remove("roboto");
+// link.classList.remove("rojo");
+link.classList.remove("azul")
 console.log("6: clases", link.classList);
 
 if (link.classList.contains("rojo")) {
@@ -29,4 +36,10 @@ if (link.classList.contains("rojo")) {
 } else {
   console.log("7: no contiene la clase rojo");
 }
-*/
+var prueba = link.classList.contains("azul");
+console.log(prueba);
+if(prueba != false){
+  console.log("8: Existe la en la clase el azul.");
+}else{
+  console.log("8: No existe en la clase el azul.");
+}
