de0baedb3fe9bb2c80ff5a89d591c7cdd05ad1aa
Gerardo González <wagegosa@gmail.com>
2020-04-16 06:02:43 -0500

    Se realiza una primera parte del ejercicio propuesto.

---
+++ 6-dom/4-manipulacion-elementos-2/script.js
@@ -0,0 +1,31 @@
+// 1: insertar elementos en diferentes posiciones
+var lista = document.getElementById("lista");
+var elemento4 = document.createElement("li");
+var elemento5 =document.createElement("li");
+elemento4.innerHTML = "Elemento 4";
+elemento5.innerHTML = "Elemento 5";
+
+/*insertAdjacentElement sirve para insertar y posicionar un elemnto. los cuatro valores son:
+ 1. afterbegin - Después del comienzo del elemento (como primer hijo).
+ 2. afterend - Después del elemento. Al final y por fuera.
+ 3. beforebegin - Antes del elemento. Al inicio y por fuera.
+ 4. beforeend - Antes del final del elemento (como último hijo)
+ */
+ lista.insertAdjacentElement("afterbegin", elemento4);
+ lista.insertAdjacentElement("beforeend", elemento5);
+
+ var titulo = document.createElement("h1");
+ var salir = document.createElement("p");
+ titulo.innerHTML = "Titulo de prueba";
+ salir.innerHTML = "Prueba";
+
+ lista.insertAdjacentElement("beforebegin", titulo);
+ lista.insertAdjacentElement("afterend", salir);
+
+ var subLista = document.getElementById("test");
+ var elemento6 =  document.createElement("div");
+ elemento6.innerHTML = "<ul> <li>Home</li> <li> Page 1</li> <li> Page 2</li> <li> Page 3</li> </ul>";
+
+ subLista.insertAdjacentElement("beforeend", elemento6);
+
+ 
